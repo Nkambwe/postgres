43e7dcefe0ef13576924b52f52f8280f0c1d86bb
jo.jomac.mac801@gmail.com <jo.jomac.mac801@gmail.com>
2018-07-09 12:30:01 +0300

    --sample test module

--- project/util.py
+++ project/util.py
@@ -1,2 +1,15 @@
+"""
+postgres/project/util.py
+"""
+
 def greet(name):
+    """
+    name: user name which must be a string type
+    """
+    if type(name) != str:
+        raise TypeError("Argument must be s string type")
+
+    if name.strip() == "":
+        raise ValueError("argument cannot be empty")
+        
     print('Hello {}'.format(name))
